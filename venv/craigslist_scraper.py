0b6a9f0b403f0bd08e0f97fc6a7da4314361a943
Nick Maude <nicholasmaude@gmail.com>
2021-05-15 19:07:43 -0400

    added craigslist scraper

--- venv/craigslist_scraper.py
+++ venv/craigslist_scraper.py
@@ -5,6 +5,10 @@ import re
 import listing
 import calendar
 import SQL_connection_info
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup
 from random import randint
@@ -15,75 +19,75 @@ mycursor = db.cursor()
 
 
 # idiomatic conversion from month abbreviation to number
-def month_to_number(s):
-    if s == 'Jan':
-        return '01'
-    if s == 'Feb':
-        return '02'
-    if s == 'Mar':
-        return '03'
-    if s == 'Apr':
-        return '04'
-    if s == 'May':
-        return '05'
-    if s == 'Jun':
-        return '06'
-    if s == 'Jul':
-        return '07'
-    if s == 'Aug':
-        return '08'
-    if s == 'Sep':
-        return '09'
-    if s == 'Oct':
-        return '10'
-    if s == 'Nov':
-        return '11'
-    if s == 'Dec':
-        return '12'
+def month_to_abriv(s):
+    if s == '01':
+        return 'Jan'
+    if s == '02':
+        return 'Feb'
+    if s == '03':
+        return 'Mar'
+    if s == '04':
+        return 'Apr'
+    if s == '05':
+        return 'May'
+    if s == '06':
+        return 'Jun'
+    if s == '07':
+        return 'Jul'
+    if s == '08':
+        return 'Aug'
+    if s == '09':
+        return 'Sep'
+    if s == '10':
+        return 'Oct'
+    if s == '11':
+        return 'Nov'
+    if s == '12':
+        return 'Dec'
     return 'N/A'
 
 
 def find_all_postings(max_pages):
     page = 1
     while page <= max_pages:
-        url = 'https://cse.google.com/cse?cx=008732268318596706411:nhtd4cwl5xu&q=laser%20sailboat&oq=laser%20sailboat&gs_l=partner-generic.3..0.3800.6459.0.7545.16.15.0.0.0.0.311.1740.7j6j1j1.15.0.csems%2Cnrl%3D13...0.2666j906674j16j1...1.34.partner-generic..4.12.1310.1WHQLX81voY'
-        source_code = requests.get(url)
-        # just the code, no headers
-        plain_text = source_code.text
-        # BeautifulSoup objects
-        soup = BeautifulSoup(plain_text, "html.parser")
+        url = 'https://clsearchengine.com/results.html?cx=000995238168550899204%3A73jyduttcf8&cof=FORID%3A10&ie=UTF-8&q=laser+sailboat#gsc.tab=0&gsc.q=laser%20sailboat&gsc.page='+str(page)
+
+        options = Options()
+        options.headless = True
+        browser = webdriver.Chrome(os.path.abspath(r'C:\Users\nicho\Desktop\chromedriver_win32\chromedriver.exe'),
+                                   options=options)
+
+
+        browser.implicitly_wait(10)
+        browser.get(url)
+        wait_for_listings = browser.find_element_by_xpath(r'//*[@id="___gcse_0"]/div/div/div/div[5]/div[2]/div/div')
+
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
 
         it = 0
         # find link to each posting
-        for link in soup.findAll('a', {'class': 'gs-title'}):
+        for postings in soup.findAll('div', {'class': 'gsc-webResult gsc-result'}):
+            link = postings.find('a', {'class': 'gs-title'})
             href = "" + link.get('href')
             # just the text, not the HTML
-            title = link.string
+            title = link.find('b').text +" "+ link.text
 
-            # shorten title if greater that 75chars
+
             if len(title) > 75:
                 title = title[0:75] + "..."
             title = '<p><a href=' + href + '>' + title + '</a></p>'
 
             # get location, year, cost, image and date posted from listing
             data = get_listing_data(href)
-            date_posted = soup.find_all('span', {'class': {'details'}})
 
-            if 'Featured Sailboat' in date_posted[it].text:
-                date_posted = date_posted[it].text[24:35]
-            else:
-                date_posted = date_posted[it].text[15:26]
-
-            year = date_posted[-4:]
-            day = date_posted[0:2]
-            month = date_posted[3:6]
-            date_posted = month + " " + day + " ," + year
-            number_date = year + "-" + month_to_number(month) + "-" + day
-
-            mycursor.execute(
-                "INSERT INTO listings (date_posted, title, location ,year ,cost ,image ,date) VALUES (%s,%s,%s,%s,%s,%s,%s)",
-                (date_posted, title, data[1], data[0], data[2], data[3], number_date)
-            )
+            # if the post is still up and its not a wanted post
+            if (data is not None) or ('wanted' not in title):
+                mycursor.execute(
+                   "INSERT INTO listings (date_posted, title, location ,year ,cost ,image ,date) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+                   (data[0], title, data[1], None, data[2], data[3], data[4])
+                )
             db.commit()
             it = it + 1
         page += 1
@@ -94,24 +98,43 @@ def get_listing_data(item_url):
     source_code = requests.get(item_url)
     plain_text = source_code.text
     soup = BeautifulSoup(plain_text, "html.parser")
+    date = soup.find('time', {'class': 'date timeago'})
 
-    # gather all bolded words
-    items = soup.findAll('font', {'face': 'Arial, Helvetica, sans-serif'})
-    image = soup.find('img', {'class': 'mainsailboatphoto'})
-    # if posting has
-    if image is not None:
-        image = "https://www.sailboatlistings.com/" + image.get('src')
+    # if the post has been removed
+    if date is None:
+        return None
+
+    numrical_date = date.text.strip()[0:10]
+    date_posted = month_to_abriv(numrical_date[5:7]) + " " + numrical_date[8:10] + " ," + numrical_date[0:4]
+
+    price = soup.find('span', {'class': 'price'})
+
+    if price is not None:
+        price = price.text
+
+    images = []
+
+    thumbs = soup.findAll('a',{'class': 'thumb'})
+    for thumb in thumbs:
+        image = thumb.get('href')
         image = '<img src =' + image + '  "Trulli" width="100" height="66">'
+        images.append(image)
 
-    # return the year, location, cost, image from the posting
-    #   year = items[6].string
-    #   location = items[10].string
-    #   cost = items[11].string
-    cost = items[11].string
+    location = soup.find('li', {"class": 'crumb area'})
+    location = location.find('a').text.title()
 
-    if cost == '$':
-        cost = None
+    if len(images) == 0:
+        images = None
     else:
-        cost = cost.replace(',', '')[1:]
+        # flatten array of html images
+        images = ' '.join(map(str, images))
 
-    return items[6].string, items[10].string, cost, image
+    if price == '$' or price is None:
+        price = None
+    else:
+        price = price.replace(',', '')[1:]
+
+    return date_posted, location, price, images, numrical_date
+
+
+
--- venv/server.py
+++ venv/server.py
@@ -1,27 +1,30 @@
 import mysql.connector
 import sailingforums_scraper
 import sailboatlistings_scraper
+import craigslist_scraper
 import buildSite
 import SQL_connection_info
 import schedule
 import upload
 from time import time, sleep
 
-
 db = SQL_connection_info.connect()
 
 mycursor = db.cursor()
+
 
 def run_server():
     rebase()
     build_site()
     upload.upload_All_Files()
 
+
 def build_site():
     buildSite.build("date")
     buildSite.build("state")
     buildSite.build("cost")
     buildSite.build("year")
+
 
 def rebase():
     print('rebaseing..')
@@ -30,6 +33,8 @@ def rebase():
         "CREATE TABLE listings (date_posted VARCHAR(50), title VARCHAR(500), location VARCHAR(500), year VARCHAR(10), cost INT(10),image TEXT(90000), date DATE)"
     )
     sailingforums_scraper.find_all_postings(8)
+    craigslist_scraper.find_all_postings(1)
     sailboatlistings_scraper.find_all_postings(1)
+
 
 run_server()
